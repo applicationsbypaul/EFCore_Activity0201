3cf982c7010d29b435c8ebf61abbe826eb87141e
PaulFord <applicationsbypaul@gmail.com>
2022-08-01 00:36:17 -0500

    use the context to pull from the database

--- EFCore_Activity0201/Program.cs
+++ EFCore_Activity0201/Program.cs
@@ -1,22 +1,55 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using EFCore_DBLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCore_Activity0201;
 internal class Program
 {
     private static IConfigurationRoot? _configuration;
+    private static DbContextOptionsBuilder<AdventureWorksContext> _optionsBuilder;
 
     static void Main(string[] args)
     {
         BuildConfiguration();
-        Console.WriteLine($"CNSTR: {_configuration.GetConnectionString("AdventureWorks")}");
+        BuildOptions();
+        ListPeople();
+        ListProducts();
     }
 
-    private static void BuildConfiguration()
+    static void BuildConfiguration()
     {
         var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: true,
             reloadOnChange: true);
         _configuration = builder.Build();
+    }
+    
+    static void BuildOptions()
+    {
+        _optionsBuilder = new DbContextOptionsBuilder<AdventureWorksContext>();
+        _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+    }
+
+    static void ListPeople()
+    {
+        using var db = new AdventureWorksContext(_optionsBuilder.Options);
+        var people = db.People.OrderByDescending(x => x.LastName).Take(20).ToList();
+
+        foreach (var person in people)
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+        }
+    }
+    
+    static void ListProducts()
+    {
+        using var db = new AdventureWorksContext(_optionsBuilder.Options);
+        var products = db.Products.Take(20).ToList();
+
+        foreach (var product in products)
+        {
+            Console.WriteLine($"{product.Name} ");
+        }
     }
 }
